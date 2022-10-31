0da3ed5bcc764bc550098ba2acfe5cf284712409
Makaukau, Tedesca T <tedesca.makaukau@standardbank.co.za>
2022-10-31 19:58:42 +0200

    refactor imports

--- src/test/java/nl/ordina/wordfrequency/controller/WordControllerTest.java
+++ src/test/java/nl/ordina/wordfrequency/controller/WordControllerTest.java
@@ -1,24 +1,16 @@
 package nl.ordina.wordfrequency.controller;
 
-import nl.ordina.wordfrequency.service.WordFrequencyAnalyzerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.servlet.ServletContext;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc

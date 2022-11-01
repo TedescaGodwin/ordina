98a099f1c40031c4da231225c17994ea8e544ebd
Makaukau, Tedesca T <tedesca.makaukau@standardbank.co.za>
2022-11-01 16:18:56 +0200

    Refactoring code and bug fix, remove contains and use custom contain function

--- src/main/java/nl/ordina/wordfrequency/service/WordFrequencyAnalyzerService.java
+++ src/main/java/nl/ordina/wordfrequency/service/WordFrequencyAnalyzerService.java
@@ -15,38 +15,26 @@ import static java.lang.Math.toIntExact;
 
 public class WordFrequencyAnalyzerService implements WordFrequencyAnalyzer
 {
-    @Override
-    public int calculateHighestFrequency(String text) {
-        List<String> wordArray = List.of(text.toLowerCase().trim().split("\\W+"));
-        int count = 0;
-        //get unique occurrence
-        var filteredList = wordArray.stream().distinct();
-        for (String word: filteredList.toList()) {
-            long currentWordCount = wordArray.stream().filter(x-> x.contains(word)).count();
-            if(currentWordCount > count) count = toIntExact(currentWordCount);
-        }
-        return count;
-    }
 
-    @Override
-    public int calculateFrequencyForWord(String text, String word) {
-        return toIntExact(Stream.of(text.toLowerCase().trim().split("\\W+"))
-                .filter(x-> x.contains(word.toLowerCase()))
-                .count());
-    }
-
-    @Override
-    public WordFrequency[] calculateMostFrequentNWords(String text, int n) {
-        List<String> wordArray = List.of(text.toLowerCase().trim().split("\\W+"));
-        if (n > (long) wordArray.size()){
+    private WordFrequency[] wordFrequencyCollection(String text){
+        var words = List.of(text.toLowerCase().trim().split("\\W+"));
+        if (words.size() <= 0){
             return new WordFrequency[]{};
         }
+
+        var filteredWords = words.stream().distinct().toArray();
         WordFrequency[] arr = new WordFrequency[0];
         var wordFrequencies = new ArrayList<WordFrequency>();
-        
-        var filteredList = wordArray.stream().distinct();
-        for (String word: filteredList.toList()) {
-            int frequency = toIntExact(wordArray.stream().filter(x -> x.contains(word)).count());
+        for (Object filteredWord : filteredWords) {
+            var word = filteredWord.toString();
+            int count = 0;
+            for (int j = 0; j < (long) words.size(); j++) {
+                if (word.equals(words.toArray()[j])) {
+                    count++;
+                }
+            }
+
+            int frequency = count;
             var wordFrequency = new WordFrequency() {
                 @Override
                 public String getWord() {
@@ -61,18 +49,34 @@ public class WordFrequencyAnalyzerService implements WordFrequencyAnalyzer
             wordFrequencies.add(wordFrequency);
         }
 
+        return wordFrequencies.toArray(arr);
+    }
+    @Override
+    public int calculateHighestFrequency(String text) {
+        return Arrays.stream(wordFrequencyCollection(text))
+                .max(Comparator.comparing(WordFrequency::getFrequency)).get().getFrequency();
+    }
+
+    @Override
+    public int calculateFrequencyForWord(String text, String word) {
+        return Arrays.stream(wordFrequencyCollection(text))
+                .filter((x-> x.getWord().toLowerCase().equals(word)))
+                .map(WordFrequency::getFrequency).findFirst().orElse(null);
+    }
+
+    @Override
+    public WordFrequency[] calculateMostFrequentNWords(String text, int n) {
+        var wordFrequencies = wordFrequencyCollection(text);
         SortByFrequencyComparator sortByFrequencyComparator = new SortByFrequencyComparator();
         SortByAscendingComparator sortByAscendingComparator = new SortByAscendingComparator();
-
         var frequencies =
-                 Arrays.stream(wordFrequencies.toArray(arr))
+                 Arrays.stream(wordFrequencies)
                 .sorted(sortByFrequencyComparator)
                 .limit(n)
                 .sorted(sortByAscendingComparator)
                          .sorted(sortByFrequencyComparator)
                 .toList();
-
-        return frequencies.toArray(arr);
+        return frequencies.toArray(new WordFrequency[0]);
     }
 
 
